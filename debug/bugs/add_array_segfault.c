916137c2858cc43a0b8b49d1a7560fb7fc3bd293
Gab223PC01 <pinguin.com>
2019-07-18 17:18:09 -0300

    segfault debuggeado

--- debug/bugs/add_array_segfault.c
+++ debug/bugs/add_array_segfault.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int add_array(int *a, int *b, int n){
   int sum = 0;
   int i = 0;
-  for (i = 0; i <= n + 1; i++) {
+  for (i = 0; i < n ; i++) {
     sum += abs(a[i]);
     sum += abs(b[i]);
   };
@@ -14,6 +15,10 @@ int main(int argc, char **argv) {
   int *a, *b;
   int n = 3;
   int i, sum;
+  
+  a = malloc(sizeof(int) * 3);
+  b = malloc(sizeof(int) * 3);
+  
   for (i = 0; i < n; i++) {
     a[i] = i;
     b[i] = i;
